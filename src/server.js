deee3c47218bd8182cef24f960cfa98481177c98
Anastasiia <anastasiianikolaienko2004@gmail.com>
2025-08-10 03:09:29 +0200

    0.1

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
-import contactsRouter from './routes/contacts.js';
+import contactsRouter from './routers/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
 const PORT = Number(getEnvVar('PORT', 3000));
