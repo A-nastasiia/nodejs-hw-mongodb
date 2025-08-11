c7fb4e2c8d5ebe86c11f734fcfbda5aa40ea4175
Anastasiia <anastasiianikolaienko2004@gmail.com>
2025-08-11 13:26:32 +0200

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,5 +1,6 @@
 import { ContactsCollection } from '../db/models/contact.js';
 
+
 export const getAllContacts = async () => {
   const contacts = await ContactsCollection.find();
   console.log('contacts:', contacts);
@@ -17,23 +18,13 @@ export const createContact = async (payload) => {
   return contact;
 };
 
-export const updateContact = async (contactId, payload, options = {}) => {
-  const rawResult = await ContactsCollection.findOneAndUpdate(
-    { _id: contactId },
+export const updateContact = async (contactId, payload) => {
+  const updatedContact = await ContactsCollection.findByIdAndUpdate(
+    contactId,
     payload,
-    {
-      new: true,
-      includeResultMetadata: true,
-      ...options,
-    },
+    { new: true },
   );
-
-  if (!rawResult || !rawResult.value) return null;
-
-  return {
-    contact: rawResult.value,
-    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
-  };
+  return updatedContact;
 };
 
 export const deleteContact = async (contactId) => {
